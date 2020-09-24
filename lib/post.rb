90068433af5440f44078d76c1911f8939a3b2895
J K <kj688895@gmail.com>
2020-09-24 18:03:02 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,12 @@
 require 'pry'
 class Post
-  attr_accessor :title, :author 
+  attr_accessor :title, :author
 
   @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+end 
