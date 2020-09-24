316fa63ded83455ab1d6154df86640f1d9c1a04c
J K <kj688895@gmail.com>
2020-09-24 18:08:36 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,8 +19,12 @@ class Author
 
   def add_post_by_title(title)
     new_post = Post.new(title)
-    new_post.author = self 
+    new_post.author = self
   end
+
+  def self.post_count
+    Post.all.count
+  end 
 
 
 end
